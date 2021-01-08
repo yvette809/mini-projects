5148e11a02ad8a0d4c030d5864ebf5bdc382a7f8
Yvette <nchombuayvta@gmil.com>
2021-01-08 23:20:17 +0100

    styling  and bugs fixes

--- buildings/src/components/VideoList.jsx
+++ buildings/src/components/VideoList.jsx
@@ -5,7 +5,12 @@ const VideoList = ({ videos, onVideoSelect }) => {
   return videos.map((video) => {
     return (
       <div className="ui relaxed divid list">
-        <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+        <VideoItem
+          key={video.id.videoId}
+          onVideoSelect={onVideoSelect}
+          video={video}
+        />
+        ;
       </div>
     );
   });
