cff23ff7a2c3e67d9a840f299d0d6a4c330da9cc
Yvette <nchombuayvta@gmil.com>
2021-02-24 15:29:39 +0100

    change background react

---
+++ react-real-estate/src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+        <>
+		<div class="hero">
+			<div class="container">
+				<h1>Welcome to my website</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, eos?</p>
+			</div>
+		</div>
+		<section class="container content">
+			<h2>content one</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eligendi commodi quos minus sunt,
+				atque dolore temporibus quam totam deserunt exercitationem ipsam voluptatum dignissimos error inventore
+				soluta magni earum? Alias enim assumenda a dolorum natus nam aperiam dolore. Tempore labore temporibus
+				et earum accusamus quam eveniet nobis amet dolorum nisi architecto nesciunt dolor, nihil, quis
+				repellendus harum fugit. Magni dicta laboriosam modi, quasi distinctio hic quidem itaque est eveniet
+				voluptate, officia sint quos. Tenetur eveniet sapiente quia provident maxime dolores dignissimos saepe,
+				aperiam voluptates maiores impedit cumque esse quas commodi error ut voluptatem, excepturi illum eum!
+				Ullam vero aspernatur atque.
+			</p>
+			<h3>Content Two</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis pariatur autem itaque
+				necessitatibus minima vero numquam quibusdam, assumenda tempore tempora.
+			</p>
+		</section>
+        </>
+	);
+};
+
+export default Home;
--- react-real-estate/src/components/Navbar.jsx
+++ react-real-estate/src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React , {useState} from 'react'
 import styled from 'styled-components'
 
 const Navbar = () => {
+
+const [nav, setNav]= useState(false)
+
+function fixNav() {
+	if (window.scrollY >= 80) {
+		setNav(true)
+	} else {
+		setNav(false)
+	}
+}
+window.addEventListener('scroll',fixNav)
+
+
+
     return (
-        <>
-        <h1>Hi</h1>
-        </>
+        // <nav className="nav">
+            <nav className={nav?'nav active': 'nav'}>
+			<div className="container">
+				<h1 className="logo"><a href="/index.html">My Website</a></h1>
+				<ul>
+					<li><a href="#" className="current">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
     )
 }
 
