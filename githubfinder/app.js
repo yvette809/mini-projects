03bdfc5616704951a9ef17b4ccea808b41aed494
Yvette <nchombuayvta@gmil.com>
2021-05-24 21:17:23 +0200

    fetch repos

--- githubfinder/app.js
+++ githubfinder/app.js
@@ -1,28 +1,33 @@
 // init github
-const github = new Github()
+const github = new Github();
+
+// init UI
+const ui = new UI();
 
 // search input
 
-const searchUser = document.getElementById("searchUser")
+const searchUser = document.getElementById("searchUser");
 
 //search input event listener
 
-searchUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener("keyup", (e) => {
     // get input text
 
     const userText = e.target.value;
     if (userText !== "") {
         //make an http call to github
-        github.getUser(userText)
-            .then(data => {
-                if(data.profile.message==='Not found'){
-
-                    // show alert
-                }else{
-                    //show profile
-                }
-            })
-    }else{
+        github.getUser(userText).then((data) => {
+            if (data.profile.message === "Not found") {
+                // show alert
+                ui.showAlert("User not found", "alert alert-danger");
+            } else {
+                //show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos)
+            }
+        });
+    } else {
         // clear profile
+        ui.clearProfile();
     }
-})
+});
--- githubfinder/github.js
+++ githubfinder/github.js
@@ -1,17 +1,43 @@
+// class Github {
+//   constructor() {
+//     this.client_id = "dc3372db4b03a6d663ee";
+//     this.client_secret = " 54eb6a8aa80e25c40b4ed8db1a77a4bf6186ef08";
+//   }
+
+//   async getUser(user) {
+//     const profileResponse = await fetch(
+//       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+//     );
+
+//     const profileData = await profileResponse.json()
+//     return{
+//         profile:profileData
+//     }
+//   }
+// }
+
 class Github {
-  constructor() {
-    this.client_id = "dc3372db4b03a6d663ee";
-    this.client_secret = " 54eb6a8aa80e25c40b4ed8db1a77a4bf6186ef08";
-  }
-
-  async getUser(user) {
-    const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
-    );
-
-    const profileData = await profileResponse.json()
-    return{
-        profile:profileData
+    constructor() {
+        this.client_id = "dc3372db4b03a6d663ee";
+        this.client_secret = " 54eb6a8aa80e25c40b4ed8db1a77a4bf6186ef08";
+        this.repos_count = 5;
+        this.repos_sort = 'created:asc'
     }
-  }
+
+    async getUser(user) {
+        const profileResponse = await fetch(
+            `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+
+        const reposResponse = await fetch(
+            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+        const profileData = await profileResponse.json()
+        const repos = await reposResponse.json()
+        return {
+            profile: profileData,
+            repos
+        }
+    }
 }
+
